3c20359e72573f5e95f9a8599a3dbfe882570f48
Or Globus <orkinyo@gmail.com>
2021-10-08 18:24:30 +0300

    article

--- moses/moses.py
+++ moses/moses.py
@@ -3,29 +3,20 @@ import re
 
 enc_msg = "x.xx...x.xxx..-xx-.xxxx.-.-xxx.-.x..x.xxxx..x.xxx.-.-.xx-.-xxx..-.xx.x.x.--x.xxx"
 
-with open('book.txt') as f:
+with open('files/book.txt') as f:
     words = re.findall(r"[a-zA-Z']+", f.read())
 
-msg = []
+flag = []
 tmp_enc_msg = enc_msg
-
-
-def match(msg, morse):
-    for i in range(len(msg)):
-        if msg[i] != 'x' and msg[i] != morse[i]:
-            return False
-    return True
-
-
 for word in words:
-    morse = mtalk.encode(word)
-    morse = morse.replace("   ", "")
-    if match(tmp_enc_msg[:len(morse)], morse):
-        msg.append(word)
-        tmp_enc_msg = tmp_enc_msg[len(morse):]
+    morse_word = mtalk.encode(word).replace("   ", "")  # remove spaces
+    enc_word = tmp_enc_msg[:len(morse_word)]
+    if all(enc_word[i] == 'x' or enc_word[i] == morse_word[i] for i in range(len(enc_word))):
+        flag.append(word)
+        tmp_enc_msg = tmp_enc_msg[len(morse_word):]
         if not tmp_enc_msg:
-            print("_".join(msg))
+            print("CSA{" + "_".join(flag) + "}")
             break
     else:
-        msg = []
+        flag = []
         tmp_enc_msg = enc_msg
